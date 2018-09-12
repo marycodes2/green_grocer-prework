e1c094db293f53630ccd50f7a1fa039b8ac6e021
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-12 23:50:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ def apply_coupons(cart, coupons)
           new_cart_dict[new_item_name] = {}
           new_cart_dict[new_item_name][:price] = coupon[:cost]
           new_cart_dict[new_item_name][:clearance] = cart[item][:clearance]
-          new_cart_dict[new_item_name][:count] = 1
+          new_cart_dict[new_item_name][:count] = coupon[:num].to_i
         end 
       end
     end
