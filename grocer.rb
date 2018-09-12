c367b356459106f0899d275a8f27c6828cbfdeeb
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-12 23:34:30 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -45,8 +45,6 @@ def apply_coupons(cart, coupons)
         new_cart_dict[new_item_name][:price] = coupon[:cost]
         new_cart_dict[new_item_name][:clearance] = cart[item][:clearance]
         new_cart_dict[new_item_name][:count] = 1
-      elsif coupons == []
-        puts "hello"
       end 
     end
   end 
