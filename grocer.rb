bf24a17dcaff56c3e89315a23e612d088bb83c79
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 01:27:30 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,11 @@ cart_hash = [
   {"KALE"    => {:price => 3.0, :clearance => false}}
 ]
 
-coupon = {:item => "AVOCADO", :num => 2, :cost => 5.0}
+coupon =     [
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "BEER", :num => 2, :cost => 20.00},
+      {:item => "CHEESE", :num => 3, :cost => 15.00}
+    ]
 
 def consolidate_cart(cart)
   count_dict = {}
