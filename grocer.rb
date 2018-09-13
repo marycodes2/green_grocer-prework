b78e09fc89df4e66424ebceb06a2ebad5d3b807b
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 01:05:35 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,6 @@
 cart_hash = {
   "PEANUTBUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
-  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3},
   "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
 }
 
