f7b5aff6b7a0b9fe9923da993f874aa1e4057020
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 02:05:01 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def apply_coupons(cart, coupons)
         coupons.each do |coupon|
         if item == coupon[:item]
           num_of_coupons_applied_per_item[item] += 1
-                    puts "number of coupons/item " + num_of_coupons_applied_per_item[item].to_s
+          puts "number of coupons/item " + num_of_coupons_applied_per_item[item].to_s
           new_item_name = "#{item} W/COUPON"
           number_of_items_leftover = new_cart_dict[item][:count].to_i - coupon[:num].to_i
           new_cart_dict[item][:price] = new_cart_dict[item][:price]
