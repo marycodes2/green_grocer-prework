d7c15c6e6f3d71c9867f7773033ce04f5d571ca1
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 01:34:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,8 +1,15 @@
-cart_hash = [
-  {"AVOCADO" => {:price => 3.0, :clearance => true }},
-  {"AVOCADO" => {:price => 3.0, :clearance => true }},
-  {"KALE"    => {:price => 3.0, :clearance => false}}
-]
+cart_hash =    [
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"KALE" => {:price => 3.00, :clearance => false}},
+      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+      {"ALMONDS" => {:price => 9.00, :clearance => false}},
+      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+      {"CHEESE" => {:price => 6.50, :clearance => false}},
+      {"BEER" => {:price => 13.00, :clearance => false}},
+      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]
 
 coupon =     [
       {:item => "AVOCADO", :num => 2, :cost => 5.00},
