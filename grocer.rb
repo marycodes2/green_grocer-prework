be35ec0544c1fddd823330c0274aac6ad21ad4cb
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 01:22:12 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 cart_hash = {
-  "PEANUTBUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
-  "KALE"         => {:price => 3.00, :clearance => false, :count => 3},
-  "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
+  "PEANUTBUTTER" => {:price => 3.00, :clearance => true},
+  "KALE"         => {:price => 3.00, :clearance => false},
+  "SOY MILK"     => {:price => 4.50, :clearance => true}
 }
 
 coupon = [
